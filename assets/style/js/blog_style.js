147c74930f1c112d3edc05f97baa3f28d71ae498
KuriSan_Fox <christophershefox@gmail.com>
2021-06-05 12:36:26 +0900

    パッチノート更新
    UTEのパッチノート更新したよ。

---
+++ assets/style/js/blog_style.js
@@ -0,0 +1,10 @@
+function blog_setup() {
+    let title_name = 0;
+    let description = 0;
+    let kisaisha = 0;
+    kisaisha = '栗君';
+    let kisaisha_id = 0;
+    kisaisha_id = 'KuriSan_Fox';
+    let kisaisha_url = 0;
+    kisaisha_url = 'https://twitter.com/KuriSan_Fox';
+}
